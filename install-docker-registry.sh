93d296be63156ffc91de6bd1b5ca79ee69f621e7
David Paterson <david_paterson@dell.com>
2021-07-23 19:05:56 -0400

    Install Helm 3 too

--- install-docker-registry.sh
+++ install-docker-registry.sh
@@ -10,4 +10,6 @@ sudo docker ps
 echo Install Kubectl cli
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo Install Helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 echo Done!
